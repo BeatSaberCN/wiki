435d6c0e9137d11fa1299fbf7ac450615135024d
frto027 <q602706150@gmail.com>
2025-12-12 16:19:18 +0800

    [no ci]update components

--- src/components/admonitions.tsx
+++ src/components/admonitions.tsx
@@ -23,6 +23,20 @@ export default {
         return return_type('caution', 标题|| title || "注意", 内容 || content || children)
     },错误({children, 标题, 内容, title, content}) {
         return return_type('danger', 标题 || title || "错误", 内容 || content || children)
+    },
+    需要扩展({children, 内容}){
+        let hint = children || 内容
+        return return_type('info', "需要扩展", 
+            hint ? ["该内容需要扩展，您可以点击页面下方的编辑按钮进行编辑。", <br/>, hint]:
+                ["该内容需要扩展，您可以点击页面下方的编辑按钮进行编辑。"]
+        )
+    },
+    网络提示({children, 内容}){
+        let hint = children || 内容
+        return return_type('info', "网络提示", 
+            hint ? ["该内容涉及的部分内容不向大陆地区提供服务。", <br />, hint]:
+                ["该内容涉及的部分内容不向大陆地区提供服务。"]
+        )
     }
 } 
 
--- src/components/doccard.tsx
+++ src/components/doccard.tsx
@@ -5,11 +5,13 @@ import {
 import DocCardList from "@theme/DocCardList"
 
 export default {
-    内容列表(){
+    内容列表({with_first}){
         const items = useCurrentSidebarSiblings();
         const filtered = []
 
-        for(let i=1;i<items.length;i++){
+        for(let i=
+            with_first ? 0 : 1
+            ;i<items.length;i++){
             filtered.push(items[i])
         }
         return <DocCardList items={filtered} />;
